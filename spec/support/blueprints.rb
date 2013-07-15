a591450a9a6a286dd957fc34b546c4b1854aeb40
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-15 15:57:48 -0400

    Update machinist blueprint.

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -2,8 +2,16 @@ require 'machinist/active_record'
 
 # Add your blueprints here.
 User.blueprint do
-  # name { Faker::Name.name}
-  email { Faker::Internet.email}
+  name { Faker::Name.name}
+  email { Faker::Internet.email }
+  password { "password" }
+  password_confirmation { "password"}
+end
+
+Merchant.blueprint do
+  name { Faker::Name.name }
+  bname { Faker::Name.name }
+  email { Faker::Internet.email }
   password { "password" }
   password_confirmation { "password"}
 end
