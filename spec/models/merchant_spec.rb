1767e03ee152955e76cd845daaf9de29775beb29
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-15 16:01:24 -0400

    Add basic rspec test for User and Merchant models.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Merchant do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should give me the correct array of matching merchant_users" do
+    merchant1 = Merchant.make!
+    merchant2 = Merchant.make!
+
+    expect(Merchant.count).to eq(2)
+
+    expect(Merchant.all).to match_array([merchant1, merchant2])
+
+  end
+
 end
