59d4a13754acfa137627d52be1be6b17b45bd033
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-15 12:03:02 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitpages::Application.config.secret_token = '375ef161ae81607cba84102e0bc78a4f2592592830ce84c2fed938e085fa5ac92faa9d9d46a30a7d4e726728d114e44437a0adeb09468f8e2ae9d5a61cd77ca2'
