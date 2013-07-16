75697119500dc76c584f1fd499bc1bc3c5443914
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-16 11:15:53 -0400

    Create cucumber tests for merchant/individual account user dashboard.

---
+++ features/step_definitions/dashboard_steps.rb
@@ -0,0 +1,42 @@
+Given(/^I am logged in as an individual user$/) do
+  visit('/users/sign_up')
+
+  name = "individual1"
+  email = "individual1@email.com"
+  password = "password"
+  password_confirmation = "password"
+
+  fill_in "user_name", :with => name
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  fill_in "user_password_confirmation", :with => password_confirmation
+
+  click_button "Create Account"
+end
+
+Then(/^I should see the individual account user dashboard when I visit the homepage$/) do
+  visit('/')
+  expect(page).to have_content("individual dashboard")
+end
+
+
+Given(/^I am logged in as a merchant user$/) do
+  visit('/users/sign_up')
+
+  name = "merchant1"
+  email = "merchant1@email.com"
+  password = "password"
+  password_confirmation = "password"
+
+  fill_in "user_name", :with => name
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  fill_in "user_password_confirmation", :with => password_confirmation
+
+  click_button "Create Account"
+end
+
+Then(/^I should see the merchant account user dashboard when I visit the homepage$/) do
+  visit('/')
+  expect(page).to have_content("merchant dashboard")
+end
