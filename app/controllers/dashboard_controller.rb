ad6c4497083a80638aa11925e116ade236c2f5d7
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-16 14:59:57 -0400

    Change dashboard partials.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    if current_user.present?
-      # @recent_merchants = Merchant.find(:all, :order => "id desc", :limit => 4)
-      @recent_merchants = Merchant.last(4).reverse
-      @new_members = User.last(3).reverse
-    elsif current_merchant.present?
-    end
+    @recent_merchants = Merchant.last(4).reverse
+    @new_members = User.last(3).reverse
   end
 end
