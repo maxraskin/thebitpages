5382d464bd588a0db707c61dbf5b79cebff6db14
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-16 12:36:10 -0400

    Add some basic markup for merchant dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,8 @@ class DashboardController < ApplicationController
       # @recent_merchants = Merchant.find(:all, :order => "id desc", :limit => 4)
       @recent_merchants = Merchant.last(4).reverse
       @new_members = User.last(3).reverse
+    elsif current_merchant.present?
+
     end
   end
 end
