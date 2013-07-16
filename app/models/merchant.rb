f37b9ead4bed64192821da5a8fe9c76022b380ec
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-16 12:55:57 -0400

    Implement geocoder method to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,13 @@ class Merchant < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :bname, :avatar, :avatar_cache, :remove_avatar, :bio, :street_address, :city, :state, :zip_code, :online_business, :bitcoin_address, :company_website, :phone, :company_affiliation, :industry, :twitter
   # attr_accessible :title, :body
+
+  geocoded_by :geocode_method
+  after_validation :geocode
+
+  def geocode_method
+    # "#{self.location} #{destination.name}"
+    
+    "#{self.street_address} #{self.city} #{self.state} #{self.zip_code}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130715230146) do
+ActiveRecord::Schema.define(:version => 20130716164539) do
 
   create_table "merchants", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(:version => 20130715230146) do
     t.string   "industry"
     t.string   "twitter"
     t.integer  "zip_code"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "merchants", ["email"], :name => "index_merchants_on_email", :unique => true
