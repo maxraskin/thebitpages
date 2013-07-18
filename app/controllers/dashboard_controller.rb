b2b3373105b496f11cb65cae0d0019cdd9cb4177
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-18 12:43:52 -0400

    Add partials for dashboard (index and search)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @recent_merchants = Merchant.last(4).reverse
+    @merchants = Merchant.last(4).reverse
     @new_members = User.last(3).reverse
   end
 
