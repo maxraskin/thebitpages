516414cf43052f453fa887d8469c98f045a92853
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-22 12:50:53 -0400

    Users can see/add/remove merchants

--- app/controllers/user_merchant_friendships_controller.rb
+++ app/controllers/user_merchant_friendships_controller.rb
@@ -1,11 +1,11 @@
 class UserMerchantFriendshipsController < ApplicationController
-  
+
   def index
     @user = current_user
   end
   
   def create
-    @friendship = current_user.user_merchant_friendships.build(:merchant_id => params[:user_id])
+    @friendship = current_user.user_merchant_friendships.build(:merchant_id => params[:merchant_id])
     if @friendship.save
       redirect_to users_url
     end
