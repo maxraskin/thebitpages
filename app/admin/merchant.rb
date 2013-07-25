6f670ef8a12f86d8431a14f49056ca027525203f
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-25 16:06:04 -0400

    Change column layout for Merchant for ActiveAdmin inteface

--- app/admin/merchant.rb
+++ app/admin/merchant.rb
@@ -1,3 +1,27 @@
 ActiveAdmin.register Merchant do
 
+  index do
+    selectable_column
+    column :id
+    column :bname
+    column :name
+    column :email
+    column :bio
+    column :street_address                     
+    column :city
+    column :state
+    column :zip_code
+    column :online_business
+    column :bitcoin_address
+    column :company_website
+    column :phone
+    column :company_affiliation
+    column :industry
+    column :twitter
+    column :latitude
+    column :longitude
+    column :current_sign_in_at        
+    column :last_sign_in_at           
+    default_actions                   
+  end 
 end
