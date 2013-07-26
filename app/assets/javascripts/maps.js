383a7caa68182310a17280a9e6e140eab9b9b71f
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-26 17:33:34 -0400

    Clicking on google markers links to merchant profile view

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,12 +2,13 @@ function initialize() {
   var myLatlng = (gon.center === undefined) ? new google.maps.LatLng(gon.merchant.latitude, gon.merchant.longitude) : new google.maps.LatLng(gon.center[0], gon.center[1])
 
   var mapOptions = {
-    zoom: 11,
+    zoom: 15,
     center: myLatlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById("merchant-google-map"), mapOptions);
+
 
 
 }
@@ -18,22 +19,29 @@ function loadScript(populateCheck) {
 
   script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAsykPdvWSTP8uKVQCv27poiNwimwVQ7ds&callback=initializeAndPopulate&sensor=false";
   document.body.appendChild(script);
+
 }
 
 function initializeAndPopulate() {
   initialize();
   populateMap();
+
 }
 
-function addMarker(latitude, longitude, letter, title) {
+function addMarker(latitude, longitude, letter, url, title) {
   // var map = new google.maps.Map(document.getElementById("merchant-google-map"));
   var myLatlng = new google.maps.LatLng(latitude, longitude);
 
   var marker = new google.maps.Marker({
     position : myLatlng,
     map: map,
-    title: title,
+    url: url,
     icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|F66F66|000000'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    window.location.href = this.url;
+    console.log("sup");
   });
 
   marker.setMap(map);
@@ -41,7 +49,9 @@ function addMarker(latitude, longitude, letter, title) {
 
 function populateMap() {
   _.each(gon.merchants, function(merchant) {
-    addMarker(merchant.latitude, merchant.longitude, merchant.bname);
+    var url = "/merchants/" + merchant.id;
+    var title = merchant.bname;
+    addMarker(merchant.latitude, merchant.longitude, merchant.bname, url, title);
   });
 }
 
