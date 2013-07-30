bfc17147193456f5f4b71680355e11c09ca6e014
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-30 13:55:29 -0400

    Add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,4 @@
 require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Bitpages::Application.initialize!
-# config.action_mailer.default_url_options = { :host => 'bitpages.heroku.com' }
 
