9898be621fcc7079215b15145dd5e21e06d21d5d
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-30 13:02:02 -0400

    Change action_mailer host

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Bitpages::Application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'bitpages.heroku.com' }
