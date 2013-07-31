9005718f402dae812c92bd01ce3ee5ec1c2e9c0f
Andrew H. Yi <andrew.yi50@gmail.com>
2013-07-31 11:46:06 -0400

    Show # of followers/followees on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,13 @@ class DashboardController < ApplicationController
   def index
     @merchants = Merchant.last(4).reverse
     @new_members = User.last(3).reverse
-    
+
     if current_merchant_profile.present?
       gon.current_user = current_merchant_profile
-    else
+      @number_of_friendships = Merchant.number_of_friendships(current_merchant_profile.merchant).to_s
+    elsif current_user.present?
       gon.current_user = current_user
+      @number_of_friendships = User.number_of_friendships(current_user)
     end
   end
 
@@ -16,10 +18,15 @@ class DashboardController < ApplicationController
     else
       gon.current_user = current_user
     end
-    
+
     if params[:zip_code].length != 5 || params[:zip_code].to_i == 0
       flash.alert = "Invalid Zip Code."
-      redirect_to root_url
+      redirect_to root_url  
+    # elsif params[:zip_code].first == "0" #fix geokit error for zip_codes with leading zeros
+    #   zip_code = params[:zip_code]
+    #   zip_code[0] = ''
+    #   zip_code = "%05d" % zip_code
+    #   binding.pry
     else
       gon.industry = @industry = params[:category]
       gon.zip_code = zip_code = params[:zip_code] 
