399e8117bfbfb477d1eadb594f59e526f33d7a41
Andrew H. Yi <andrew.yi50@gmail.com>
2013-08-01 10:13:34 -0400

    Remove merchants emails from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   
   def index
-    @recent_merchants = Merchant.last(3).reverse
+    @recent_merchants = Merchant.last(4).reverse
     offset = rand(Merchant.count)
     gon.merchant = rand_merchant = Merchant.first(:offset => offset)
-
     gon.merchants = @merchant_array = Merchant.within(10, :origin => rand_merchant)
-
   end
 end
