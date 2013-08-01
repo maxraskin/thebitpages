8818a4273bc3400855bf9693246eb6936cffe23e
Andrew H. Yi <andrew.yi50@gmail.com>
2013-08-01 16:25:38 -0400

    Add infowindow

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -10,7 +10,6 @@ function initialize() {
   map = new google.maps.Map(document.getElementById("merchant-google-map"), mapOptions);
 
 
-
 }
 
 function loadScript(populateCheck) {
@@ -36,12 +35,25 @@ function addMarker(latitude, longitude, letter, url, title, icon_number) {
     position : myLatlng,
     map: map,
     url: url,
+    title: title,
     icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld="+icon_number+"|F66F66|000000"
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: title,
+    size: new google.maps.Size(87,50)
   });
 
   google.maps.event.addListener(marker, 'click', function() {
     window.location.href = this.url;
-    console.log("sup");
+  });
+
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(map, marker);
+  });
+
+  google.maps.event.addListener(marker, 'mouseout', function() {
+    infowindow.close();
   });
 
   marker.setMap(map);
@@ -51,7 +63,7 @@ function populateMap() {
   var icon_number = 1;
   _.each(gon.merchants, function(merchant) {
     var url = "/merchants/" + merchant.id;
-    var title = merchant.bname;
+    var title = merchant.bname+ ": " + merchant.city + ", " + merchant.state;
     addMarker(merchant.latitude, merchant.longitude, merchant.bname, url, title, icon_number);
     icon_number += 1;
   });
