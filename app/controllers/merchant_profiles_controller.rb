46c2f70b12722e1beea0e68f3f3426c1061678e4
Andrew H. Yi <andrew.yi50@gmail.com>
2013-08-13 12:02:34 -0400

    Add recaptcha

--- app/controllers/merchant_profiles_controller.rb
+++ app/controllers/merchant_profiles_controller.rb
@@ -6,36 +6,40 @@ class MerchantProfilesController < Devise::RegistrationsController
   end
 
   def create
-    if params[:merchant_profile][:merchant_id].present?
-      @merchant = Merchant.find(params[:merchant_profile][:merchant_id])
-      @merchant_profile = MerchantProfile.create(params[:merchant_profile])
 
-      @merchant_profile.name = @merchant.name
-      @merchant_profile.bio = @merchant.bio
-      @merchant_profile.street_address = @merchant.street_address
-      @merchant_profile.city = @merchant.city
-      @merchant_profile.state = @merchant.state
-      @merchant_profile.zip_code = @merchant.zip_code
-      @merchant_profile.online_business = @merchant.online_business
-      @merchant_profile.bitcoin_address = @merchant.bitcoin_address
-      @merchant_profile.company_website = @merchant.company_website
-      @merchant_profile.industry = @merchant.industry
-      @merchant_profile.twitter = @merchant.twitter
-      @merchant_profile.latitude = @merchant.latitude
-      @merchant_profile.longitude = @merchant.longitude
-      @merchant_profile.save
-    else
-      @merchant_profile = MerchantProfile.create(params[:merchant_profile])
-      @merchant = Merchant.new(params[:merchant_profile])
-      @merchant.save
+    if verify_recaptcha
+      if params[:merchant_profile][:merchant_id].present?
+        @merchant = Merchant.find(params[:merchant_profile][:merchant_id])
+        @merchant_profile = MerchantProfile.create(params[:merchant_profile])
 
-      @merchant_profile.merchant = @merchant
-      @merchant.merchant_profile = @merchant_profile
+        @merchant_profile.name = @merchant.name
+        @merchant_profile.bio = @merchant.bio
+        @merchant_profile.street_address = @merchant.street_address
+        @merchant_profile.city = @merchant.city
+        @merchant_profile.state = @merchant.state
+        @merchant_profile.zip_code = @merchant.zip_code
+        @merchant_profile.online_business = @merchant.online_business
+        @merchant_profile.bitcoin_address = @merchant.bitcoin_address
+        @merchant_profile.company_website = @merchant.company_website
+        @merchant_profile.industry = @merchant.industry
+        @merchant_profile.twitter = @merchant.twitter
+        @merchant_profile.latitude = @merchant.latitude
+        @merchant_profile.longitude = @merchant.longitude
+        @merchant_profile.save
+      else
+        @merchant_profile = MerchantProfile.create(params[:merchant_profile])
+        @merchant = Merchant.new(params[:merchant_profile])
+        @merchant.save
 
-      @merchant_profile.save
+        @merchant_profile.merchant = @merchant
+        @merchant.merchant_profile = @merchant_profile
+
+        @merchant_profile.save
+      end
+      redirect_to merchant_path(@merchant)
+    else 
+      redirect_to new_merchant_profile_registration_path
     end
-
-    redirect_to merchant_path(@merchant)
   end
 
   def update
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LdlB-YSAAAAADjguAE_j9KIiu4CD9seIEthtUQ6'
+  config.private_key = '6LdlB-YSAAAAAFQus_-bw_oofMUsge2KVsHM3saD'
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
